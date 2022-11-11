bcdee9a2b59b75ba129b7026ce3e2d107b4e1049
Jordan Mata <jordanmata15@gmail.com>
2022-11-10 19:15:47 -0800

    Implemented reading test data from file.

--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -1,10 +1,10 @@
-import keras
 import numpy as np
+import os
+import pandas as pd
 import random
-import tensorflow as tf
 
-from keras.models import Sequential
-from keras.layers import Dense
+SRC_DIR = os.path.dirname(os.path.realpath(__file__))
+TRAINING_DATA_DIR = os.path.join(os.path.join(SRC_DIR, ".."), "model_data")
 
 def generate_n_random_inputs(n):
     values = []
@@ -13,6 +13,7 @@ def generate_n_random_inputs(n):
     x1_values = values[:n]
     x2_values = values[n:]
     return np.array(list(zip(x1_values, x2_values)))
+
 
 def calculate_half_adder_outputs(input_pairs):
     outputs = []
@@ -24,12 +25,31 @@ def calculate_half_adder_outputs(input_pairs):
         outputs.append([sum, carry_bit])
     return np.array(outputs)
 
+
 def generate_n_random_input_outputs(n):
     inputs = generate_n_random_inputs(n)
     outputs = calculate_half_adder_outputs(inputs)
     return [inputs, outputs]
 
 
+def write_to_file(input_data, output_data, filename):
+    df_inputs =  pd.DataFrame(input_data, columns=['x1','x2'])
+    df_outputs = pd.DataFrame(output_data, columns=['y1','y2'])
+    df_inputs_outputs = df_inputs
+    df_inputs_outputs[['y1', 'y2']] = df_outputs
+    df_inputs_outputs.to_csv(os.path.join(TRAINING_DATA_DIR, filename), index=False)
+
+
+def read_from_file(filename):
+    df = pd.read_csv(os.path.join(TRAINING_DATA_DIR, filename), index_col=False)
+    print(df)
+    inputs = df[['x1','x2']].to_numpy()
+    outputs = df[['y1','y2']].to_numpy()
+    return [inputs, outputs]
+
+
 if __name__ == "__main__":
     data_size = 100
     inputs, outputs = generate_n_random_input_outputs(100)
+    write_to_file(inputs, outputs, "training_data.csv")
+    inputs, outputs = read_from_file("training_data.csv")
