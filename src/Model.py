fbeb3f0bcb984e61982cb9106004bcb9463b6138
Jordan Mata <jordanmata15@gmail.com>
2022-11-14 11:36:09 -0800

    Finalized model.

--- src/Model.py
+++ src/Model.py
@@ -1,6 +1,7 @@
 import keras
 import numpy as np
 import os
+import pandas as pd
 import tensorflow as tf
 
 from keras.models import Sequential
@@ -11,6 +12,7 @@ import DataGenerator
 
 SRC_DIR = os.path.dirname(os.path.realpath(__file__))
 MODELS_DIR = os.path.join(os.path.join(SRC_DIR, ".."), "models")
+HISTORY_DIR = os.path.join(os.path.join(SRC_DIR, ".."), "training_history")
 
 def build_model():
     learning_rate=0.1
@@ -20,9 +22,16 @@ def build_model():
 
     model = Sequential()
     # 3 neurons, 2 inputs, dense means fully connected 
-    model.add(Dense(3, activation='relu', input_dim=input_size))
-    model.add(Dense(3, activation='relu'))
-    model.add(Dense(output_size, activation='sigmoid'))
+    model.add(Dense(3, 
+                    activation='relu', 
+                    input_dim=input_size, 
+                    bias_initializer=keras.initializers.Constant(0.1)))
+    #model.add(Dense(3, 
+    #                activation='relu', 
+    #                bias_initializer=keras.initializers.Constant(0.1)))
+    model.add(Dense(output_size, 
+                    activation='sigmoid', 
+                    bias_initializer=keras.initializers.Constant(0.1)))
 
     #my_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     my_optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=momentum)
@@ -32,7 +41,7 @@ def build_model():
 
 
 def train(input, output, model):
-    model.fit(input, output, epochs=100, batch_size=1)
+    return model.fit(input, output, epochs=100, batch_size=1)
 
 
 def validate(predicted_labels, expected_labels):
@@ -42,11 +51,10 @@ def validate(predicted_labels, expected_labels):
         num_total += 1
         if (predicted[0] == expected[0]) and (predicted[1] == expected[1]):
             num_correct += 1
-            print(predicted, "==", expected)
         else:
             print(predicted, "!=", expected)
-            pass
     return num_correct / num_total
+
 
 def predict(model, test_input):
     return np.round(model.predict(test_input), 0).astype(int)
@@ -60,26 +68,25 @@ def save_model(model, model_name):
 
 
 if __name__ == "__main__":
-    #x_train, y_train = DataGenerator.read_from_file("training_data.csv")
     skew = 0.2
     x_train, y_train = DataGenerator.generate_n_random_input_outputs(200, skew)
     x_test, y_test = DataGenerator.generate_n_random_input_outputs(200, skew)
-    
-    #x_train, x_test, y_train, y_test = train_test_split(inputs, outputs)
 
     if not True:
         model_name = "Initial_model"
         model = keras.models.load_model(os.path.join(MODELS_DIR, model_name))
     else:
         model = build_model()
-        train(x_train, y_train, model)
-        save_model(model, "Second_model")
+        history = train(x_train, y_train, model)
+        save_model(model, "generated_model")
+        history_df = pd.DataFrame.from_dict(history.history)
+        history_df.index.name = "epoch"
+        history_df.to_csv(os.path.join(HISTORY_DIR, "history.csv"))
+
 
     predicted_labels = predict(model, x_test)
-    print(zip(x_test, predicted_labels))
     correct_pct = validate(predicted_labels, y_test)
     print(correct_pct)
-    print(model.predict([[-0.08,0.1]]))
     
     DataGenerator.write_to_file(x_train, y_train, "training.csv")
     #print(y_test)
